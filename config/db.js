9055854cd5a849abbab49ee1ec8f28a061410c0c
Franco Chong <fnbeddie@hotmail.com>
2017-03-09 19:54:38 -0500

    minor changes again

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
   "URI": "mongodb://localhost/contacts"
-  //Remote MongoDB deployment -> let URI = "mongodb://franco:12345>@ds145329.mlab.com:45329/contacts";
+  //Remote MongoDB deployment -> let URI = "mongodb://franco:12345@ds145329.mlab.com:45329/contacts";
 };
